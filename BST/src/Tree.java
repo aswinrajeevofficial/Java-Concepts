ba7dd6cda87a936dc832746fa8cc0504a5a1bd15
aswinrajeevofficial <aswinworkofficial@gmail.com>
2020-05-13 23:35:35 +0530

    Tree Insertion/Traversals

---
+++ BST/src/Tree.java
@@ -0,0 +1,78 @@
+
+public class Tree {
+
+	public Node root;
+
+	Tree() {
+		this.root = null;
+	}
+
+	public static void main(String[] args) {
+		Tree tree = new Tree();
+		tree.insertNode(8);
+		tree.insertNode(4);
+		tree.insertNode(12);
+		tree.insertNode(1);
+		tree.insertNode(7);
+		tree.insertNode(9);
+		tree.insertNode(14);
+		System.out.println("Inorder");
+		tree.printInOrder(tree.root);
+		System.out.println("\nPreOrder");
+		tree.printPreOrder(tree.root);
+		System.out.println("\nPostorder");
+		tree.printPostOrder(tree.root);
+	}
+
+	public void insertNode(int data) {
+		root = insertHelper(root, data);
+	}
+
+	public Node insertHelper(Node root, int data) {
+		if (root == null) {
+			root = new Node(data);
+			return root;
+		}
+		if (data < root.getData()) {
+			root.setLeft(insertHelper(root.getLeft(), data));
+		}
+		if (data > root.getData()) {
+			root.setRight(insertHelper(root.getRight(), data));
+		}
+		return root;
+
+	}
+
+	public void printInOrder(Node root) {
+		if(root == null) {
+			return;
+		}
+		else {
+			printInOrder(root.getLeft());
+			System.out.print(root.getData() + " -> ");
+			printInOrder(root.getRight());
+		}
+	}
+
+	public void printPreOrder(Node root) {
+		if(root == null) {
+			return;
+		}
+		else {
+			System.out.print(root.getData() + " -> ");
+			printPreOrder(root.getLeft());
+			printPreOrder(root.getRight());
+		}
+	}
+
+	public void printPostOrder(Node root) {
+		if(root == null) {
+			return;
+		}
+		else {
+			printPostOrder(root.getLeft());
+			printPostOrder(root.getRight());
+			System.out.print(root.getData() + " -> ");			
+		}
+	}
+}
