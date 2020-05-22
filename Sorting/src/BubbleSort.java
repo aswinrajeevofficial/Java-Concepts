05de156b82e8527f5055bf0db0c68d883f2845fa
aswinrajeevofficial <aswinworkofficial@gmail.com>
2020-05-22 21:03:06 +0530

    Bubble Sort explanation added in comments

--- Sorting/src/BubbleSort.java
+++ Sorting/src/BubbleSort.java
@@ -25,3 +25,30 @@ public class BubbleSort {
 		}
 	}
 }
+
+/*
+Explanation of Bubble Sort:
+Initial Array: [5, 4, 2, 6, 1]
+arr.length = 5
+The outer loop will iterate from 0 to 4
+The inner loop will iterate from 0 to length-i-1
+
+Let's do a dry run of the first iteration
+j will iterate from 0 to 4-0-1 = 3
+j = 0
+if(arr[j+1] < arr[j]) -> 4 < 5 is true, swap them
+Array will be: 4, 5, 2, 6, 1
+j = 1
+if(arr[j+1] < arr[j]) -> 2 < 5 is true, swap them
+Array will be: 4, 2, 5, 6, 1
+j = 2
+if(arr[j+1] < arr[j]) -> 6 < 5 is not true, not swapping them
+j = 3 Last iteration
+if(arr[j+1] < arr[j]) -> 1 < 6, is true, swap them
+Array will be: 4, 2, 5, 1, 6
+
+This is the array after first iteration. As you can see, with bubble sort
+after the first iteration, the largest element in the array will be at the 
+end index
+
+*/
